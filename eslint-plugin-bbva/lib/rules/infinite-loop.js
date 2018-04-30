afbdc6c928811b4a8aead472276e479b6c2837eb
josex2r <codex2r@gmail.com>
2018-04-30 09:31:24 +0200

    fix assignment expression inside update expression in a for loop

--- eslint-plugin-bbva/lib/rules/infinite-loop.js
+++ eslint-plugin-bbva/lib/rules/infinite-loop.js
@@ -19,7 +19,9 @@ module.exports = {
 				if (init.type === 'VariableDeclaration') {
 					const vars = init.declarations.map((declarator) => declarator.id && declarator.id.name).filter(Boolean);
 
-					if (update.argument.type === 'Identifier' && !vars.includes(update.argument.name)) {
+					if (update.argument && update.argument.type === 'Identifier' && !vars.includes(update.argument.name)) {
+						context.report(node, '"for" loop incrementers should modify the variable being tested in the loop\'s stop condition');
+					} else if (update.type === 'AssignmentExpression' && !vars.includes(update.left.name)) {
 						context.report(node, '"for" loop incrementers should modify the variable being tested in the loop\'s stop condition');
 					}
 				}
