befa68261d607f3d821d6cf2d6d9c1ea8b78ac0f
Alberto Matias <alberto.matias@bbva.com>
2019-12-30 08:58:56 +0100

    feat(prefer-throw-in-promise): doc

---
+++ packages/eslint-plugin-bbva/tests/lib/rules/prefer-throw-in-promise.js
@@ -0,0 +1,95 @@
+'use strict';
+
+const rule = require('../../../lib/rules/prefer-throw-in-promise');
+const RuleTester = require('eslint').RuleTester;
+
+const ruleTester = new RuleTester({
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2018
+	}
+});
+
+ruleTester.run('prefer-throw-in-promise', rule, {
+	valid: [
+		'import RSVP, { reject } from \'rsvp\'; foo().then(() => { throw new Error(); });',
+		'import { resolve } from \'rsvp\'; foo().then(() => { throw new Error(); });',
+		'foo().then(() => { throw new Error(); });',
+		'foo().then(() => {}, () => { throw new Error(); });',
+		'foo().catch(() => { throw new Error(); });',
+		'try { foo(); } catch(e) { throw new Error(); }'
+	],
+
+	invalid: [
+		{
+			code: 'import { reject } from \'rsvp\'; foo().then(() => { return reject() });',
+			errors: [{
+				message: 'throw an Error instead of returning a rejected promise',
+				type: 'CallExpression'
+			}]
+		},
+		{
+			code: 'import RSVP from \'rsvp\'; foo().then(() => { return RSVP.reject() });',
+			errors: [{
+				message: 'throw an Error instead of returning a rejected promise',
+				type: 'CallExpression'
+			}]
+		},
+		{
+			code: 'import { reject as alias } from \'rsvp\'; foo().then(() => { return alias() });',
+			errors: [{
+				message: 'throw an Error instead of returning a rejected promise',
+				type: 'CallExpression'
+			}]
+		},
+		{
+			code: 'import alias from \'rsvp\'; foo().then(() => { return alias.reject() });',
+			errors: [{
+				message: 'throw an Error instead of returning a rejected promise',
+				type: 'CallExpression'
+			}]
+		},
+		{
+			code: 'foo().then(() => { return Promise.reject() });',
+			errors: [{
+				message: 'throw an Error instead of returning a rejected promise',
+				type: 'CallExpression'
+			}]
+		},
+		{
+			code: 'import { reject } from \'rsvp\'; function bar() { try { foo(); } catch(e) { return reject(); } }',
+			errors: [{
+				message: 'throw an Error instead of returning a rejected promise',
+				type: 'CallExpression'
+			}]
+		},
+		{
+			code: 'import RSVP from \'rsvp\'; function bar() { try { foo(); } catch(e) { return RSVP.reject(); } }',
+			errors: [{
+				message: 'throw an Error instead of returning a rejected promise',
+				type: 'CallExpression'
+			}]
+		},
+		{
+			code: 'import { reject as alias } from \'rsvp\'; function bar() { try { foo(); } catch(e) { return alias(); } }',
+			errors: [{
+				message: 'throw an Error instead of returning a rejected promise',
+				type: 'CallExpression'
+			}]
+		},
+		{
+			code: 'import alias from \'rsvp\'; function bar() { try { foo(); } catch(e) { return alias.reject(); } }',
+			errors: [{
+				message: 'throw an Error instead of returning a rejected promise',
+				type: 'CallExpression'
+			}]
+		},
+		{
+			code: 'function bar() { try { foo(); } catch(e) { return Promise.reject(); } }',
+			errors: [{
+				message: 'throw an Error instead of returning a rejected promise',
+				type: 'CallExpression'
+			}]
+		}
+	]
+});
