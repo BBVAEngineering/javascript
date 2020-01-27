976feb0ed0a2e1d90925ec8f1abede70adc04fd6
adrigzr <adrigzr@users.noreply.github.com>
2020-01-27 14:55:11 +0100

    fix(config): add external plugins declaration

--- packages/eslint-config-bbva/index.js
+++ packages/eslint-config-bbva/index.js
@@ -1,16 +1,12 @@
 module.exports = {
 	extends: [
 		'./rules/basic.json',
-		'./rules/error.json',
 		'./rules/best-practices.json',
-		'./rules/variables.json',
-		'./rules/node.json',
-		'./rules/issues.json',
+		'./rules/error.json',
 		'./rules/es6.json',
-		'./rules/external.json'
-	].map(require.resolve),
-
-	rules: {
-		// O_O
-	}
+		'./rules/external.json',
+		'./rules/issues.json',
+		'./rules/node.json',
+		'./rules/variables.json'
+	].map(require.resolve)
 };
